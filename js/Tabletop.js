2c63b23d5eecfaf6acbb5c7f098d9a9f04991eb7
Rodolfo Garcia <rodolfosg@gmail.com>
2021-03-30 00:59:19 -0300

    new functionality add heroes and buttons hover

--- js/Tabletop.js
+++ js/Tabletop.js
@@ -1,29 +1,41 @@
-const hero = document.querySelector('.hero');
+var heroes = document.querySelectorAll('.hero');
+const addHeroButton = document.querySelector('.addHero');
+const addMonsterButton = document.querySelector('.addMonster');
 
-hero.addEventListener('mousedown', mouseDown);
+addHeroButton.addEventListener('mousedown', addHero);
+addHeroButton.addEventListener('mouseover', overAddHero);
+addMonsterButton.addEventListener('mousedown', addMonster);
+addMonsterButton.addEventListener('mouseover', overAddMonster);
+
+
+
+
+addHero();
+
+
+
 
 function mouseDown(e) {
-    console.log('down')
+    //get X and Y position and mouse click
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+    var hero_action = this;
     
     window.addEventListener('mousemove', mouseMove);
     window.addEventListener('mouseup', mouseUp);
 
-    //get X and Y position and mouse click
-    let prevX = e.clientX;
-    let prevY = e.clientY;
-
+    
     function mouseMove(e) {
-        console.log('moving..')
         //update X and Y position of object relative with mouse click
         let newX = prevX - e.clientX;
         let newY = prevY - e.clientY;
 
         //get the current properties from Hero (position)
-        const heroRect = hero.getBoundingClientRect();
+        let heroRect = hero_action.getBoundingClientRect();
 
         //update the position of Hero
-        hero.style.left = heroRect.left - newX + "px";
-        hero.style.top = heroRect.top - newY + "px";
+        hero_action.style.left = heroRect.left - newX + "px";
+        hero_action.style.top = heroRect.top - newY + "px";
 
         //update previous X and Y
         prevX = e.clientX;
@@ -31,8 +43,37 @@ function mouseDown(e) {
     }
 
     function mouseUp() {
-        console.log('up')
         window.removeEventListener('mousemove', mouseMove);
         window.removeEventListener('mouseup', mouseUp);
     }
+}
+
+function mouseOver() {
+    console.log('over');
+}
+
+function addHero() {
+    newDiv = document.createElement('div');
+    newDiv.className = 'hero';
+    newDiv.style.top = '500px';
+    newDiv.style.left = '100px';
+    document.querySelector('.board').append(newDiv);
+    heroes = document.querySelectorAll('.hero');
+    for (let hero of heroes) {
+        hero.addEventListener('mousedown', mouseDown);
+        hero.addEventListener('mouseover', mouseOver);
+        console.log(heroes.length)
+    }
+}
+
+function addMonster() {
+    console.log('added monster');
+}
+
+function overAddHero() {
+    console.log('over add hero');
+}
+
+function overAddMonster() {
+    console.log('over add mon');
 }
